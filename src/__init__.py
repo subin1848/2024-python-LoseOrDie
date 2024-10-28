287f7545ce02e141db6973ecaed34fb57c44957e
subin1848 <s2315@e-mirim.hs.kr>
2024-10-29 00:25:29 +0900

    🎉[feat] init

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+# 화면 크기 설정
+WIDTH = 1920
+HEIGHT = 1024
+FPS = 60
+
+# 배경 이미지
+BACKGROUND_IMAGES = {
+    1: "assets/background/gym.png",
+    2: "assets/background/myHome.png",
+    # 2: "assets/background/myHome.png",
+}
